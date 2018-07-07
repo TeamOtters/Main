74b2f216913d55a2b12456364c54bfb63cf6f111
ravenblackhart <raven@blackhart.info>
2018-07-07 20:26:16 +0200

    Stats Screen Created

---
+++ TeamOtters/Assets/Code/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour {
+
+	
+    public void OnClick()
+ 
+
+        {
+            //The SceneManager loads your new Scene as a single Scene (not overlapping). This is Single mode.
+            SceneManager.LoadScene("Menu_Screen", LoadSceneMode.Single);
+        }
+   
+
+
+
+
+    
+}
