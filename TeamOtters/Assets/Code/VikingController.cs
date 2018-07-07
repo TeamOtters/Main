1fc9eeaa80ce5744bbb710af6b07c60bbc57829d
TwoFeathers <contact@twofeathersstudio.com>
2018-07-07 19:20:55 +0200

    viking update

--- TeamOtters/Assets/Code/VikingController.cs
+++ TeamOtters/Assets/Code/VikingController.cs
@@ -9,7 +9,6 @@ public class VikingController : MonoBehaviour {
     private VikingProjectiles m_vikingProjectiles;
     public GameObject m_projectile;
 
-
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
@@ -19,29 +18,37 @@ public class VikingController : MonoBehaviour {
     public float forceY;
     public float projectileSpeed;
 
+
+    private float m_rapidFireSpeed = 0.2f;
+    private bool m_fireCooldownOn;
+
+    private
+
+
     // Use this for initialization
     void Start () {
 
         m_vikingcCharacterController = GetComponent<CharacterController>();
-
-
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-       
 
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetButtonDown("Fire1_P1"))
         {
-
+            if (m_fireCooldownOn)
+                return;
             //if (m_projectile != null)
             //{
                var projectile = Instantiate(Resources.Load(m_projectile.name, typeof(GameObject)), new Vector3 (transform.position.x + 0.4f, transform.position.y +0.4f, transform.position.z), transform.rotation) as GameObject;
 
            // Debug.Log(m_vikingProjectiles.m_projectile.name);
                projectile.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(forceX, forceY, 0) * projectileSpeed);
-            StartCoroutine("DestroyProjectileTimer",(projectile));
+           // StartCoroutine("DestroyProjectileTimer",(projectile));
+
+            m_fireCooldownOn = true;
+            StartCoroutine("FireCoolDown");
             //}
 
 
@@ -54,11 +61,11 @@ public class VikingController : MonoBehaviour {
 
         if (m_vikingcCharacterController.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,0);
+            moveDirection = new Vector3(Input.GetAxis("Horizontal_P1"), 0,0);
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
 
-            if (Input.GetButton("Jump"))
+            if (Input.GetButtonDown("Jump_P1"))
                 moveDirection.y = jumpSpeed;
 
         }
@@ -67,6 +74,12 @@ public class VikingController : MonoBehaviour {
 
     }
 
+    IEnumerator FireCoolDown ()
+    {
+        yield return new WaitForSeconds(m_rapidFireSpeed);
+        m_fireCooldownOn = false;
+            
+    }
 
     IEnumerator DestroyProjectileTimer(GameObject _objectToDestroy)
     {
