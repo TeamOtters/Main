373f8da6817b4de15a482cc6488d22ff14b415d9
ravenblackhart <raven@blackhart.info>
2018-07-08 01:00:30 +0200

    Updated SceneChanger to work with both MainGame & Menu_Screen

--- TeamOtters/Assets/Code/SceneChanger.cs
+++ TeamOtters/Assets/Code/SceneChanger.cs
@@ -1,23 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class SceneChanger : MonoBehaviour {
+public class SceneChanger : MonoBehaviour
+{
+    public void Start()
+    {
+        // Create a temporary reference to the current scene.
+        Scene currentScene = SceneManager.GetActiveScene();
 
-	
-    public void OnClick()
- 
+        // Retrieve the name of this scene.
+        string sceneName = currentScene.name;
 
+    }
+
+    public void onClick()
+
+
+    { 
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Menu_Screen"))
+        { 
+            SceneManager.LoadScene("MainGame", LoadSceneMode.Single);
+        }
+
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MainGame"))
         {
-            //The SceneManager loads your new Scene as a single Scene (not overlapping). This is Single mode.
             SceneManager.LoadScene("Menu_Screen", LoadSceneMode.Single);
         }
-   
+    }
 
 
-
-
-    
 }
