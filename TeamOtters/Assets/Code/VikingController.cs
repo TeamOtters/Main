2d9ec8f151c724d4ca231e90d8f5f730a7120ca3
TwoFeathers <contact@twofeathersstudio.com>
2018-07-07 17:38:34 +0200

    VikingMechanics

---
+++ TeamOtters/Assets/Code/VikingController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VikingController : MonoBehaviour {
+
+    private float m_vikingSpeedForce;
+    private CharacterController m_vikingcCharacterController;
+    private VikingProjectiles m_vikingProjectiles;
+    public GameObject m_projectile;
+
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    public float forceX;
+    public float forceY;
+    public float projectileSpeed;
+
+    // Use this for initialization
+    void Start () {
+
+        m_vikingcCharacterController = GetComponent<CharacterController>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+
+            //if (m_projectile != null)
+            //{
+               var projectile = Instantiate(Resources.Load(m_projectile.name, typeof(GameObject)), new Vector3 (transform.position.x + 0.4f, transform.position.y +0.4f, transform.position.z), transform.rotation) as GameObject;
+
+           // Debug.Log(m_vikingProjectiles.m_projectile.name);
+               projectile.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(forceX, forceY, 0) * projectileSpeed);
+            StartCoroutine("DestroyProjectileTimer",(projectile));
+            //}
+
+
+           
+ 
+        }
+
+
+      
+
+        if (m_vikingcCharacterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        m_vikingcCharacterController.Move(moveDirection * Time.deltaTime);
+
+    }
+
+
+    IEnumerator DestroyProjectileTimer(GameObject _objectToDestroy)
+    {
+        yield return new WaitForSeconds(5f);
+        DestroyProjectile(_objectToDestroy);
+    }
+
+    private void DestroyProjectile(GameObject _objectToDestroy)
+    {
+        Destroy(this);
+    }
+}
---
+++ TeamOtters/Assets/Code/VikingProjectiles.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu (fileName = "Projectile")]
+public class VikingProjectiles : ScriptableObject {
+
+    public GameObject m_projectile;
+    [HideInInspector]
+    public string m_prefabLoadPath;
+    public float m_force;
+    public float m_damage;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_prefabLoadPath = m_projectile.name;
+	}
+
+    public void InstantiateProjecte ()
+    {
+
+    }
+    
+
+
+    IEnumerator SelfDestroyProjectileTimer()
+    {
+        yield return new WaitForSeconds(10f);
+        SelfDestroyer();
+    }
+	
+	private void SelfDestroyer ()
+    {
+        Destroy(this);
+    }
+}
