751f49d42328f2e63a577edcb6ad98d5df934cba
TwoFeathers <contact@twofeathersstudio.com>
2018-07-08 11:16:56 +0200

    Current scene for bouncing ball and starting score system. WIP

---
+++ TeamOtters/Assets/Code/BouncingBall.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BouncingBall : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float XBounceSpeed = 0f;
+    public float YBounceSpeed = 0f;
+
+
+
+    public float startHealth = 100;
+    private float health; 
+    public int worth = 50;
+    public GameObject deathEffect;
+
+    public Image healthBar; 
+
+    void Start()
+    {
+        rb.velocity = new Vector2(XBounceSpeed, YBounceSpeed);
+        health = startHealth;
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+     {
+     if (collisionInfo.gameObject.CompareTag ("Axe"))
+       {
+          Debug.Log(collisionInfo.gameObject.tag);
+       }
+     }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+}
