7ca94323fdbf6b18721c2466d5cfdb80abc1cf1d
TwoFeathers <contact@twofeathersstudio.com>
2018-07-08 21:44:55 +0200

    Elvira: Viking now has axe sprite, axe rotation, stone, projectile behaviour can fetch some kind of score, ignores player - but stun other players etc.

---
+++ TeamOtters/Assets/Code/ProjectileBehaviour.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour {
+
+    private Rigidbody m_rigidBody;
+    private Collider m_collider;
+    public int m_playerID;
+    public float m_rotationSpeed;
+    private bool m_hit;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+        EnableRagdoll();
+        m_collider = GetComponent<Collider>();
+       
+    }
+
+    private void Update()
+    {
+
+        //transform.rotation = Quaternion.Euler(50 * Time.deltaTime,0, 0);
+        if(!m_hit)
+            transform.Rotate(0, 0, m_rotationSpeed * Time.deltaTime, Space.Self);
+
+        // transform.Rotate(new Vector3(1, 0, 0));
+    }
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.collider.CompareTag("Player"))
+        {
+            DisableRagdoll();
+           
+            //    Physics.IgnoreCollision(collision.collider, this.transform.GetComponent<Collider>());
+
+
+        }
+
+        if(collision.collider.CompareTag("Player"))
+        {
+            if (collision.collider.gameObject.GetComponent<PlayerData>().myPlayerIndex == m_playerID)
+                Physics.IgnoreCollision(GetComponent<Collider>(), collision.collider, true);
+        }
+
+        if(collision.collider.CompareTag("Scoreable"))
+        {
+            Debug.Log("CollectScore!");
+        }
+    }
+    
+    private void EnableRagdoll()
+    {
+        if (m_rigidBody != null)
+        {
+            m_rigidBody.isKinematic = false;
+            m_rigidBody.detectCollisions = true;
+        }
+    }
+     private void DisableRagdoll()
+    {
+        if (m_rigidBody != null)
+        {
+            m_rigidBody.isKinematic = true;
+            m_rigidBody.detectCollisions = false;
+            Destroy(m_collider);
+            m_hit = true;
+        }
+    }
+
+}
--- TeamOtters/Assets/Code/VikingController.cs
+++ TeamOtters/Assets/Code/VikingController.cs
@@ -7,7 +7,9 @@ public class VikingController : MonoBehaviour {
     private float m_vikingSpeedForce;
     private CharacterController m_vikingcCharacterController;
     private VikingProjectiles m_vikingProjectiles;
+    private PlayerData m_playerData;
     public GameObject m_projectile;
+    public GameObject m_hand;
 
     public float m_vikingMovementSpeed = 6.0F;
     public float m_vikingJumpSpeed = 8.0F;
@@ -18,78 +20,174 @@ public class VikingController : MonoBehaviour {
     public float m_projectileForceY;
     public float m_projectileSpeed;
 
-
     private float m_rapidFireSpeed = 0.2f;
     private bool m_fireCooldownOn;
-    private int m_playerIndex;
+    public bool m_isStunned;
+
+    public float m_stunnedCoolDown = 1f;
+
+    private int m_thisPlayerIndex;
 
 
     // Use this for initialization
     void Start () {
 
         m_vikingcCharacterController = GetComponent<CharacterController>();
-        m_playerIndex = transform.parent.GetComponent<PlayerData>().m_PlayerIndex;
+        m_playerData = GetComponentInParent<PlayerData>();
+        if(m_playerData == null)
+         m_playerData = GetComponent<PlayerData>();
+
+        if (m_playerData != null)
+            m_thisPlayerIndex = m_playerData.myPlayerIndex;
     }
-	
-	// Update is called once per frame
-	void Update () {
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_isStunned)
+        {
+            VikingFire();
+            VikingMovement();
+        }
 
-        if (Input.GetButtonDown("Fire1_P" + m_playerIndex))
+        // Only do work if meaningful
+        /*  if (vNewInput.sqrMagnitude < 0.1f)
+          {
+              return;
+          }*/
+  
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Projectile"))
+        {
+            if(collision.gameObject.GetComponent<ProjectileBehaviour>().m_playerID != m_thisPlayerIndex)
+             SetStunned();
+
+            
+        }
+    }
+
+    private void VikingFire ()
+    {
+
+        Vector3 vNewInput = new Vector3(Input.GetAxis("Horizontal_P1"), Input.GetAxis("Vertical_P1"), 0.0f);
+        var angle = Mathf.Atan2(Input.GetAxis("Horizontal_P1"), Input.GetAxis("Vertical_P1")) * Mathf.Rad2Deg;
+        //  Debug.Log(angle);
+
+        //Fire Projectile
+        if (Input.GetButtonDown("Fire1_P1"))
         {
             if (m_fireCooldownOn)
                 return;
 
-               var projectile = Instantiate(Resources.Load(m_projectile.name, typeof(GameObject)), new Vector3 (transform.position.x + 0.4f, transform.position.y +0.4f, transform.position.z), transform.rotation) as GameObject;
-
-            float angle = Mathf.Atan2(Input.GetAxis("Horizontal_P" + m_playerIndex), Input.GetAxis("Vertical_P" + m_playerIndex));
             //char.transform.eulerAngles = new vector3(char.transform.eulerAngles.x, Mathf.atan2(x, y) * Mathf.rad2deg, char.transform.eulerAngles.z);
-            projectile.transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
-            projectile.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(m_projectileForceX, m_projectileForceY, 0) * m_projectileSpeed);
-            
+
+
+            //Up
+            if (Mathf.Clamp(angle, -10, 10) == angle)
+            {
+                
+                var projectile = Instantiate(Resources.Load(m_projectile.name, typeof(GameObject)), new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z), transform.rotation) as GameObject;
 
 
 
+                //projectile.transform.rotation = Quaternion.Euler(angle, 0, 0);
+                projectile.transform.rotation = Quaternion.Euler(0, 0, 0);
+                projectile.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(angle, m_projectileForceY, 0) * m_projectileSpeed * m_projectileForceY);
+                projectile.gameObject.GetComponent<ProjectileBehaviour>().m_playerID = m_thisPlayerIndex;
 
-            Debug.Log(angle);
+               
+                StartCoroutine("DestroyProjectileTimer", projectile);
 
+                //Debug.Log("Up");
+                //Debug.Log(angle);
+            }
 
+            //Right
+            else if (Mathf.Clamp(angle, 10, 170) == angle)
+            {
+                var projectile = Instantiate(Resources.Load(m_projectile.name, typeof(GameObject)), new Vector3(transform.position.x + 1f, transform.position.y + 0.5f, transform.position.z), transform.rotation) as GameObject;
+                //projectile.transform.rotation = Quaternion.Euler(angle, 0, 0);
+                projectile.transform.rotation = Quaternion.Euler(0, 0, 0);
+                projectile.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(angle, m_projectileForceY, 0) * m_projectileSpeed);
+                projectile.gameObject.GetComponent<ProjectileBehaviour>().m_playerID = m_thisPlayerIndex;
 
-            StartCoroutine("DestroyProjectileTimer",projectile);
+                projectile.GetComponent<SpriteRenderer>().flipX = true;
+                
+                StartCoroutine("DestroyProjectileTimer", projectile);
+
+                // Debug.Log("Right");
+                // Debug.Log(angle);
+            }
+
+            //Left
+            else if (Mathf.Clamp(angle, -170, -10) == angle)
+            {
+                var projectile = Instantiate(Resources.Load(m_projectile.name, typeof(GameObject)), new Vector3(transform.position.x - 1f, transform.position.y + 0.5f, transform.position.z), transform.rotation) as GameObject;
+                //projectile.transform.rotation = Quaternion.Euler(angle, 0, 0);
+                projectile.transform.rotation = Quaternion.Euler(0, 0, 0);
+                projectile.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(angle, m_projectileForceY, 0) * m_projectileSpeed);
+                projectile.gameObject.GetComponent<ProjectileBehaviour>().m_playerID = m_thisPlayerIndex;
+                projectile.GetComponent<SpriteRenderer>().flipX = false;
+
+               
+                // Debug.Log("Left");
+                // Debug.Log(angle);
+
+                StartCoroutine("DestroyProjectileTimer", projectile);
+            }
+
+            //Down
+            else if (angle == 180)
+            {
+                if (m_vikingcCharacterController.isGrounded)
+                    return;
+
+                var projectile = Instantiate(Resources.Load(m_projectile.name, typeof(GameObject)), new Vector3(transform.position.x, transform.position.y - 2f, transform.position.z), transform.rotation) as GameObject;
+                // projectile.transform.rotation = Quaternion.Euler(angle, 0, 0);
+                projectile.transform.rotation = Quaternion.Euler(0, 0, 0);
+                projectile.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(0, m_projectileForceY * -1, 0) * m_projectileSpeed * m_projectileForceY);
+                projectile.gameObject.GetComponent<ProjectileBehaviour>().m_playerID = m_thisPlayerIndex;
+                
+                //  Debug.Log("Down");
+                // Debug.Log(angle);
+
+                StartCoroutine("DestroyProjectileTimer", projectile);
+            }
 
             m_fireCooldownOn = true;
             StartCoroutine("FireCoolDown");
 
-        } 
+        }
+    }
 
-
-      
-
+    private void VikingMovement ()
+    {
         if (m_vikingcCharacterController.isGrounded)
         {
-            m_vikingMoveDirection = new Vector3(Input.GetAxis("Horizontal_P" + m_playerIndex), 0,0);
+            m_vikingMoveDirection = new Vector3(Input.GetAxis("Horizontal_P1"), 0, 0);
             m_vikingMoveDirection = transform.TransformDirection(m_vikingMoveDirection);
             m_vikingMoveDirection *= m_vikingMovementSpeed;
 
-            if (Input.GetButtonDown("Jump_P" + m_playerIndex))
+            if (Input.GetButtonDown("Jump_P1"))
                 m_vikingMoveDirection.y = m_vikingJumpSpeed;
 
         }
+
+        // m_hand.transform.Translate(new Vector3 (angle, 0, 0) * 10f * Time.deltaTime);
+        //m_hand.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
         m_vikingMoveDirection.y -= m_vikingGravityForce * Time.deltaTime;
         m_vikingcCharacterController.Move(m_vikingMoveDirection * Time.deltaTime);
-
-
-   
-
-       
-
     }
+
 
     IEnumerator FireCoolDown ()
     {
         yield return new WaitForSeconds(m_rapidFireSpeed);
         m_fireCooldownOn = false;
-            
     }
 
     IEnumerator DestroyProjectileTimer(GameObject _objectToDestroy)
@@ -100,6 +198,20 @@ public class VikingController : MonoBehaviour {
 
     private void DestroyProjectile(GameObject _objectToDestroy)
     {
-        Destroy(_objectToDestroy);
+        if(_objectToDestroy != null)
+         Destroy(_objectToDestroy);
+    }
+
+    public void SetStunned ()
+    {
+        m_isStunned = true;
+        Invoke("StunnedCooldown", m_stunnedCoolDown);
+        Debug.Log("STUNNED");
+    }
+
+    private void StunnedCooldown ()
+    {
+        Debug.Log("NotStunned!");
+        m_isStunned = false;
     }
 }
