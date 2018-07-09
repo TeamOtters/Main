338c5447b12ccf3c64fef283e1b0d1277d34c65f
ravenblackhart <raven@blackhart.info>
2018-07-10 01:47:21 +0200

    Vivienne : Fixed TimeOut

--- TeamOtters/Assets/TimeOut.cs
+++ TeamOtters/Assets/TimeOut.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class TimeOut : MonoBehaviour {
     public Canvas m_TimeOut;
+    
 
-    private void Start()
+    void Start()
     {
+        StartCoroutine("Wait");
         Time.timeScale = 0.0f;
+             
     }
 
-    void Update () {
-        if (Time.frameCount == 35f)
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSecondsRealtime(1.5f);
+        m_TimeOut.gameObject.SetActive(false);
+        Time.timeScale = 1.0f;
+        
+    }
+
+
+   /* void Update () {
+        if (gameObject.)
         {
             m_TimeOut.gameObject.SetActive(false);
             Time.timeScale = 1.0f;
         }
-    }
+    }*/
     
 
 }
