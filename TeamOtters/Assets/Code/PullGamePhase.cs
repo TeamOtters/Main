fae2adda6ecd605796a937b136c08b8d81aec250
ravenblackhart <raven@blackhart.info>
2018-07-10 01:08:37 +0200

    Vivienne : TimeOut for Phase1 Ready_Canvas set; Game Paused when main screen loads until Ready_Canvas deactivated.

--- TeamOtters/Assets/Code/PullGamePhase.cs
+++ TeamOtters/Assets/Code/PullGamePhase.cs
@@ -8,6 +8,7 @@ using UnityEngine.Events;
 
 public class PullGamePhase : MonoBehaviour {
 
+
     public Scene m_Active;
     public GameObject m_PullGamePhase;
 
---
+++ TeamOtters/Assets/TimeOut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeOut : MonoBehaviour {
+    public Canvas m_TimeOut;
+
+    private void Start()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    void Update () {
+        if (Time.frameCount == 35f)
+        {
+            m_TimeOut.gameObject.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+    
+
+}
+
+   
