9313444e7b5d76a76bf2e205488028b4d61abb03
holmlundjulia <40901249+holmlundjulia@users.noreply.github.com>
2018-07-11 12:39:44 +0200

    Julia: Added score on the valkyrie attack making a valkyrie drop a viking

--- TeamOtters/Assets/Code/ValkyrieAttackDetect.cs
+++ TeamOtters/Assets/Code/ValkyrieAttackDetect.cs
@@ -5,22 +5,44 @@ using UnityEngine;
 public class ValkyrieAttackDetect : MonoBehaviour {
     
     private ValkyrieController m_valkyrie;
+    private GameController m_gameController;
+    private ScoreManager m_scoreManager;
+    private PlayerData m_playerData;
 
     // Use this for initialization
     void OnStart ()
     {
         m_valkyrie = GetComponentInParent<ValkyrieController>();
+        m_gameController = GameController.Instance;
+        m_scoreManager = m_gameController.m_scoreManager;
+        m_playerData = GetComponentInParent<PlayerData>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Valkyrie")
         {
-            // Make other valkyrie drop pickup if carrying and hit
-            other.GetComponent<ValkyrieController>().DropPickup(); //should change if we do a shield mechanic
-            other.GetComponent<ValkyrieController>().GetStunned();
+            ValkyrieController otherValkyrieController = other.GetComponent<ValkyrieController>();
 
-            Debug.Log("OMG YOU HIT A VALKYRIE!");
+            if (otherValkyrieController.isCarrying == true)
+            {
+                // Make other valkyrie drop pickup if carrying and hit
+                other.GetComponent<ValkyrieController>().DropPickup(); //should change if we do a shield mechanic
+
+                // Add to the score of the valkyrie when you successfully attack another valkyrie
+                if (m_scoreManager != null)
+                {
+                    m_scoreManager.AddToScore(m_gameController.hitOpponent, m_playerData.m_PlayerIndex);
+                    Debug.Log("I hit a valkyrie and got " + m_gameController.hitOpponent + "points!");
+                }
+                else
+                {
+                    Debug.Log("Score manager is null");
+                }
+            }
+            otherValkyrieController.GetStunned();
+
+            
         }
     }
 }
