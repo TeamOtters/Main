c0e88d4013098010a580114a11bded41889d6343
holmlundjulia <40901249+holmlundjulia@users.noreply.github.com>
2018-07-11 15:26:35 +0200

    Julia: Camera behavior now pans from right to left (rotation will come after respawn viking mechanic). Also added a script that will later contain viking respawn logic.

--- TeamOtters/Assets/Code/CameraBehaviourManager.cs
+++ TeamOtters/Assets/Code/CameraBehaviourManager.cs
@@ -8,11 +8,24 @@ public class CameraBehaviourManager : MonoBehaviour {
     [SerializeField]
     private bool m_valkyrieRaceState;
 
-    public float m_panSpeed = 0.1f;
+    public float m_panSpeedY = 0.1f;
+    public float m_panSpeedXMin = 0.1f;
+    public float m_panSpeedXMax = 0.1f;
     public GameObject m_endPanTarget;
+    public float m_panXAmount;
+
+    private float targetX;
+
+    
+    /* Rotation stuff, let's deal with this later
+    public float targetYRotation = 5f;
+    public float rotationSpeed = 1f;
+    */
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
+        targetX = m_panXAmount;
     }
 
     public void SetRaceState (bool enable)
@@ -27,10 +40,42 @@ public class CameraBehaviourManager : MonoBehaviour {
         if(m_valkyrieRaceState)
         {
             Vector3 pos = Camera.main.transform.position;
+            Vector3 rot = Camera.main.transform.rotation.eulerAngles;
             Vector3 target = m_endPanTarget.transform.position;
-            Vector3 move = new Vector3(0, target.y * m_panSpeed * Time.deltaTime, 0);
-           
+            
+
+            float maxDistance = m_panXAmount * 2;
+            float percentageOfMax = targetX / maxDistance;
+            // clamp the value to 0-1 so we don't have to do a comparison
+            percentageOfMax = Mathf.Clamp01(percentageOfMax);
+            // if you were using lerp to change the speed...
+
+            float dynamicSpeed;
+
+            dynamicSpeed = Mathf.Lerp(m_panSpeedXMin, m_panSpeedXMax, percentageOfMax);
+            
+            Vector3 move = new Vector3(targetX * dynamicSpeed, target.y * m_panSpeedY * Time.deltaTime, 0);
+
+            //Vector3 move = new Vector3(0f * m_panSpeedX, target.y * m_panSpeedY * Time.deltaTime, 0);
+
             transform.Translate(move, Space.World);
+
+
+            /* Rotation stuff, let's deal with this later
+            float step = rotationSpeed * Time.deltaTime;
+            Vector3 targetRotation = new Vector3(0f,Time.deltaTime * rotationSpeed, 0f);
+
+            Camera.main.transform.Rotate(targetRotation, Space.Self);
+
+           
+            if (rot.y == targetYRotation)
+                targetYRotation = -targetYRotation;
+            */
+   
+
+            //Set the new target x value for moving from left to right
+            if (Mathf.Clamp(pos.x, targetX - 0.5f, targetX + 0.5f) == pos.x)
+                targetX = -targetX;
 
             //Stop Camera pan when we reached goal
             if (Mathf.Clamp(pos.y, target.y - 0.5f, target.y + 0.5f) == pos.y)
---
+++ TeamOtters/Assets/VikingRespawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VikingRespawn : MonoBehaviour
+{
+    private VikingController m_vikingController;
+    private GameController m_gameController;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_gameController = GameController.Instance;
+        m_vikingController = GetComponent<VikingController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
