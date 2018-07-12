f7471fab87a1aec55cd7d26c9bc1e9779088ad8f
ravenblackhart <raven@blackhart.info>
2018-07-12 03:39:23 +0200

    Vivienne : more ui stuffus

---
+++ TeamOtters/Assets/Code/MenuAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System;
+
+public class MenuAnim : Selectable
+{
+    BaseEventData m_base;
+    private bool m_menuShine=false;
+
+    public void Start()
+    {
+        m_menuShine = GetComponentInChildren<Button>().; 
+    }
+
+    public void Update() { 
+    {
+        if (IsHighlighted(m_base))
+        {
+            Debug.Log ( gameObject.name + "is Highlighted");         }
+    }
+
+   
+}
+}
+
