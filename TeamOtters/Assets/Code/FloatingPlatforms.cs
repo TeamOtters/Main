44d54d421930c05d9a40c00cdde2155ade93c35f
TwoFeathers <contact@twofeathersstudio.com>
2018-07-13 19:36:38 +0200

    elvira: floaty platforms cloud

--- TeamOtters/Assets/Code/FloatingPlatforms.cs
+++ TeamOtters/Assets/Code/FloatingPlatforms.cs
@@ -7,6 +7,9 @@ public class FloatingPlatforms : MonoBehaviour {
 
     public PlatformType m_platformType;
     public UnityEvent m_onCollisionEvent;
+    private float m_originalPos;
+    private bool m_bounce;
+
 
     public enum PlatformType
     {
@@ -15,24 +18,49 @@ public class FloatingPlatforms : MonoBehaviour {
 
     }
 
-	// Use this for initialization
-	void Start () {
-
-        if (m_onCollisionEvent == null)
-            m_onCollisionEvent = new UnityEvent();
+    private void Start()
+    {
+        m_originalPos = this.transform.position.y;
+        m_rb = GetComponent<Rigidbody>();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void Update()
     {
-        if(collision.collider.gameObject.CompareTag("Viking"))
+        if (m_platformType == PlatformType.Cloud)
+           CloudBounce();
+            
+
+       
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        
+        if (col.gameObject.CompareTag("Viking"))
         {
-            if(m_platformType == PlatformType.Cloud)
-            {
-                m_onCollisionEvent.Invoke();
-                Debug.Log("CLOUD");
-            }
+            m_bounce = true;
+            //m_onCollisionEvent.Invoke();
+
+          
+          
+
+            Debug.Log("CLOUD");
+            
         }
     }
+
+    private bool m_invoked;
+    private void CloudBounce()
+    {
+        var cloudMoveDist = 0.5f;
+ 
+
+            transform.position = new Vector3(transform.position.x,
+            m_originalPos + ((float)Mathf.Sin(Time.time) * cloudMoveDist),
+            0);
+    }
+
+  
 
    
 }
