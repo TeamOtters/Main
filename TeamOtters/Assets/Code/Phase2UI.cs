c8382767be95864d0173f28d53320730f00599ea
ravenblackhart <raven@blackhart.info>
2018-07-13 17:11:01 +0200

    update

--- TeamOtters/Assets/Code/Phase2UI.cs
+++ TeamOtters/Assets/Code/Phase2UI.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
 
-public class Phase2UI: MonoBehaviour {
+public class Phase2UI : MonoBehaviour
+{
     private GameController m_gameController;
     private ScoreManager m_scoreManager;
     public Canvas m_Phase2;
@@ -19,7 +20,13 @@ public class Phase2UI: MonoBehaviour {
 
     public float[] m_rankScale;
 
-    private Vector3 targetScale;  
+    private Vector3 targetScale;
+
+   // public GameObject NSprite;
+    public GameObject HSprite;
+   // public Button scoreBoard;
+
+
 
 
     // Use this for initialization
@@ -47,28 +54,32 @@ public class Phase2UI: MonoBehaviour {
 
     public void Update()
     {
-        for (int i=0; i<m_scoreManager.m_ranks.Count; i++)
+        for (int i = 0; i < m_scoreManager.m_ranks.Count; i++)
         {
+            Debug.Log(i);
             targetScale = new Vector3(m_rankScale[i], m_rankScale[i], 1);
             Text scaleText = m_scoreManager.scoreText[(m_scoreManager.m_ranks[i].playerIndex) - 1];
-            Transform scaleV = scaleText.transform.parent; 
+            Transform scaleV = scaleText.transform.parent;
             scaleV.localScale = new Vector3(scaleV.localScale.x * targetScale.x, scaleV.localScale.y * targetScale.y, 1);
 
-            
+            if (i==0)
+            {
+                HSprite.transform.parent = scaleV;
+                HSprite.transform.position = scaleV.transform.position;
+                HSprite.transform.localScale = scaleV.transform.localScale; 
+            }
+
+
         }
 
-        if (m_scoreManager.m_ranks[0].playerIndex == 1)
-        {
-            m_scoreManager.scoreText[(m_scoreManager.m_ranks[0].playerIndex) - 1].transform.parent.GetComponent<Button>().Select();
-        }
-
+       
     }
 
 
     private IEnumerator RankDelay(int index)
     {
         yield return new WaitForSeconds(0.5f);
-        if (index < (m_scoreManager.scoreText.Length)-1)
+        if (index < (m_scoreManager.scoreText.Length) - 1)
         {
             index++;
             ActivateScoreboard(index);
@@ -78,9 +89,19 @@ public class Phase2UI: MonoBehaviour {
 
     public void ActivateScoreboard(int index)
     {
-        m_scoreManager.scoreText[(m_scoreManager.m_ranks[index].playerIndex)-1].transform.parent.gameObject.SetActive(true);
+        m_scoreManager.scoreText[(m_scoreManager.m_ranks[index].playerIndex) - 1].transform.parent.gameObject.SetActive(true);
         StartCoroutine(RankDelay(index));
     }
 
+    /*public void ChangeImage()
+    {
+        if (scoreBoard.image.sprite == NSprite)
+            scoreBoard.image.sprite = HSprite;
+        else
+        {
+            scoreBoard.image.sprite = NSprite;
+        }
+
+    }*/
 }
 
